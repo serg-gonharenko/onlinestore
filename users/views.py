41018cb699c05459d1678758d784f61e1e770847
serg <serg.goncharenko.odessa@gmail.com>
2022-08-14 12:21:05 +0300

    Добавлена аутентификация пользователя.

--- users/views.py
+++ users/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpRequest, HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse_lazy
+from django.contrib.auth import authenticate, login
 
 from onlinestore.views import context
 
-
+# TODO переписать user_profile
 def user_profile(request: HttpRequest, username: str) -> HttpResponse:
     users_list = context["users_list"]
     for logged_user in users_list:
@@ -15,5 +16,12 @@ def user_profile(request: HttpRequest, username: str) -> HttpResponse:
 
 def login_view(request: HttpRequest) -> HttpResponse:
     """Login user view"""
-    context = {}
-    return render(request, "user_login.html", context)
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is None:
+            return HttpResponseRedirect(reverse_lazy("login"))
+        login(request, user)
+        return HttpResponseRedirect(reverse_lazy("homepage"))
+    return render(request, "user_login.html")
